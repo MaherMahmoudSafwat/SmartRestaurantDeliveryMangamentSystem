69bbbfdfe1e6ccc5372367002eadbf35b922a272
MaherMs2001 <MaherMsSoftware2001@gmail.com>
2026-06-27 14:34:52 +0300

    The project has been created

---
+++ wajba-resaurantSmartDeliveryManagementSystem-api/src/main/java/com/wajba/restaurant/WajbaResaurantSmartDeliveryManagementSystemApiApplication.java
@@ -0,0 +1,13 @@
+package com.wajba.restaurant;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WajbaResaurantSmartDeliveryManagementSystemApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WajbaResaurantSmartDeliveryManagementSystemApiApplication.class, args);
+	}
+
+}
---
+++ wajba-resaurantSmartDeliveryManagementSystem-api/src/test/java/com/wajba/restaurant/WajbaResaurantSmartDeliveryManagementSystemApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.wajba.restaurant;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WajbaResaurantSmartDeliveryManagementSystemApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
